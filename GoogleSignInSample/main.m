d12ad0af265fa916732f648361497a4be53ec0d8
加藤 雄大 <takahiro.kato15@gmail.com>
2015-09-13 01:35:44 +0900

    Initial Commit

---
+++ GoogleSignInSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoogleSignInSample
+//
+//  Created by 加藤 雄大 on 2015/09/13.
+//  Copyright (c) 2015年 grandbig.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
